9180e106c0c97f5d2eac12257c0a2c7b97ee43a9
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-05-15 21:44:18 +0530

    Add files via upload

---
+++ Example7.java
@@ -0,0 +1,12 @@
+package com.blc.elc;
+
+public class Example7 {
+
+	public static void main(String[] args) {
+	    for (int i=1;i<=100;i++){
+	      if (i%4!=0){
+	        System.out.print(i+" ");
+	      }
+	    }
+	  }
+	}
